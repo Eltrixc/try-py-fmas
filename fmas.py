83317929f710dee7cb3b863002adfb135f09c3a5
Eltrixc <eltrocco150@gmail.com>
2025-02-10 21:22:57 +0700

    Update fmas.py

--- fmas.py
+++ fmas.py
@@ -1,6 +1,1 @@
-git clone https://github.com/USERNAME/simulasi-optik.git
-cd simulasi-optik
-echo "Hello GitHub!" > hello.txt
-git add hello.txt
-git commit -m "Menambahkan file hello.txt"
-git push origin main
+
